0fe9472cb8f51fe1495b97258b2c2c4bbbf5677e
TheElixZammuto <theelixzammuto@gmail.com>
2024-07-17 21:25:35 +0200

    .

--- src/nvenc/nvenc_base.cpp
+++ src/nvenc/nvenc_base.cpp
@@ -327,6 +327,7 @@ namespace nvenc {
         format_config.singleSliceIntraRefresh = 1;
         format_config.intraRefreshPeriod = 300;
         format_config.intraRefreshCnt = 299;
+        //:-)
         break;
       }
 
